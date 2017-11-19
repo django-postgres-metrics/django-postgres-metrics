4e4a790e5a9b044b36b5cc5b139a13950fbad1e5
Markus Holtermann <info@markusholtermann.eu>
2017-11-19 16:53:59 -0500

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+version = __import__('postgres_stats').__version__
+
+
+setup(
+    name='django-postgres-stats',
+    version=version,
+    url='https://github.com/MarkusH/django-postgres-stats',
+    author='Markus Holtermann',
+    author_email='info@markusholtermann.eu',
+    description='',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['Django>=1.11'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
