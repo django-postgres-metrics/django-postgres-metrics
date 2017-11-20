c7eb330d35cf3181fd689449f6e7ec1021802587
Markus Holtermann <info@markusholtermann.eu>
2017-11-20 08:00:39 -0500

    muti database support

--- postgres_stats/views.py
+++ postgres_stats/views.py
@@ -1,8 +1,33 @@
-from django.db import connection
+from django.db import connections
+from django.core.exceptions import PermissionDenied
 from django.http import Http404
 from django.shortcuts import render
 
 from .constants import STATS
+
+
+class Statistic:
+
+    def __init__(self, connection, name):
+        self._connection = connection
+        self._get_stats(name)
+
+    def __repr__(self):
+        return '<Database "%s">' % self._connection.alias
+
+    @property
+    def alias(self):
+        return self._connection.alias
+
+    @property
+    def dsn(self):
+        return self._connection.connection.dsn
+
+    def _get_stats(self, name):
+        with self._connection.cursor() as cursor:
+            cursor.execute(STATS[name])
+            self.headers = [c.name for c in cursor.description]
+            self.records = cursor.fetchall()
 
 
 def stats_view(request, name):
@@ -12,15 +37,15 @@ def stats_view(request, name):
     if name not in STATS:
         raise Http404
 
-    with connection.cursor() as cursor:
-        cursor.execute(STATS[name])
-        headers = [c.name.replace('_', ' ') for c in cursor.description]
-        data = cursor.fetchall()
-
     context = {
-        'result_headers': headers,
-        'results': data,
+        'statistics': [],
         'stats_name': name,
     }
+
+    for connection in connections.all():
+        if connection.vendor != 'postgresql':
+            continue
+        db = Statistic(connection, name)
+        context['statistics'].append(db)
 
     return render(request, 'postgres_stats/table.html', context=context)
